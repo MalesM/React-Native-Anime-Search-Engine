9384b2f65c88f277d4b4555abae645ab736a9ece
Pavle <jopica123@gmail.com>
2017-12-14 23:56:38 +0100

    Test Externi plejer

--- android/app/src/main/java/com/animetitan/MainApplication.java
+++ android/app/src/main/java/com/animetitan/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.wog.videoplayer.VideoPlayerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new OrientationPackage(),
-            new RNAdMobPackage()
+            new RNAdMobPackage(),
+             new VideoPlayerPackage()
       );
     }
 
